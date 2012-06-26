d4dcc8524da4767b465409672b65f815b508b4b1
John <john@john.com>
2012-06-26 17:44:34 +0800

    refresh workspace

--- wp-content/themes/l1/includes/thumbnail.php
+++
@@ -1,7 +0,0 @@
-<div class="thumb">
-	<?php if ( has_post_thumbnail() ) { ?>
-		<a target="_blank" href="<?php the_permalink() ?>"><?php the_post_thumbnail('thumbnail', array( 'alt' => trim(strip_tags( $post->post_title )), 'title' => trim(strip_tags( $post->post_title )),'class' => 'alignleft')); ?></a>
-	<?php } else { ?>
-		<a target="_blank" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><img src="<?php echo get_featcat_image(); ?>" class="alignleft" alt="<?php the_title(); ?>" /></a>
-	<?php } ?>
-</div>
